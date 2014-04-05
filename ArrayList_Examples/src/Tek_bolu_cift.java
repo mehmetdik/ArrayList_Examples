ccacb96c560d02792c69ecadb3596c9a0eaa7194
mehmetdik <mehmet_dik_@hotmail.com>
2014-04-05 23:54:58 +0300

    JAVA

---
+++ ArrayList_Examples/src/Tek_bolu_cift.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+
+public class Tek_bolu_cift {
+
+
+	public static void main(String[] args) 
+	{
+
+		ArrayList<Integer> hesap =new ArrayList<Integer>();
+
+		for (int i = 0; i < 100; i++)
+		{
+			hesap.add(i);
+		}
+		
+        System.out.println("Sonuc="+Hesaplar(hesap));
+
+
+	}
+	public static double Hesaplar( ArrayList<Integer>hesap)
+	{
+		double çifttop=0;
+		double tektop=0;
+		for (int i = 0; i < 100; i++)
+		{
+		if (hesap.get(i)%2==0) {
+			çifttop=çifttop+hesap.get(i);
+		}
+		else if (hesap.get(i)%2==1)
+		{
+			tektop=tektop+hesap.get(i);
+		}		
+		}
+		return çifttop/tektop;
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+ Parametre olarak bir ArrayList alan ve bu ArrayList’teki tek sayıların toplamının çift sayıların toplamına oranını döndüren metodu yazınız.
